19a3e606ccdf7ce1da9b20370046c8addd287466
Mushkela <andrewgendy9@gmail.com>
2023-05-16 01:43:49 -0400

    Removed Heroku and SQLite. Added PostgreSQL

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = "django-insecure-62wypm56o^l$c@2f6r!f^!(n(8fc9g$j5p-a#foxc41gq)dd(%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pleasehelp-v1.herokuapp.com']
+# ALLOWED_HOSTS = ['pleasehelp-v1.herokuapp.com']
 
 
 # Application definition
@@ -79,10 +78,21 @@ WSGI_APPLICATION = "main.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pleasehelp',
+        'USER': 'postgres',
+        'PASSWORD': '0184570470',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
@@ -121,8 +131,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
-django_heroku.settings(locals())
 
 STATIC_URL = "static/"
 STATIC_DIR = BASE_DIR / "static"
